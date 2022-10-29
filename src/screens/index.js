109d68c17c03c30bf2af56dd37704a116f2ab1df
amol_magar <magaramol09@gmail.com>
2022-10-30 00:16:08 +0530

    added : Notes & Checklist screens

--- src/screens/index.js
+++ src/screens/index.js
@@ -1,3 +1,5 @@
 export { OnboardingScreen } from "./Onboarding.jsx";
 export { LogIn } from "./LogIn.jsx";
-export { Home } from './Home'
+export { Home } from "./Home";
+export { Checklist } from "./Checklist.jsx";
+export { Notes } from "./Notes.jsx";
